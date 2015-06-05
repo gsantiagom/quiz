da5d696b4e5ef436f67d072cbca72ad8a5f74a39
gsantiagom <turmus@hotmail.com>
2015-06-05 11:04:32 +0200

    BBDD dos preguntas
    Hecho en creditos por error

--- models/models.js
+++ models/models.js
@@ -36,8 +36,13 @@ sequelize.sync().then(function() {
 	// success(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function ( count){
 		if (count === 0) { // la tabla se inicializa solo si está vacía
-			Quiz.create({ pregunta: 'Capital de Italia',
+			Quiz.create({ id: 1,
+						  pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
+			});
+			Quiz.create({ id: 2, 
+						  pregunta: 'Capital de Portugal',
+						  respuesta: 'Lisboa'
 			})
 			.then(function() {console.log('Base de datos inicializada')});
 		};
