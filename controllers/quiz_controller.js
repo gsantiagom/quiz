2470f043d34603f3a1a737d1892915a0bd6dbe2a
gsantiagom <turmus@hotmail.com>
2015-06-11 11:42:11 +0200

    Creacion de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -44,6 +44,25 @@ exports.answer = function(req, res) {
 	})
 };
 
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( // crea objeto quiz
+	  { pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+
+	res.render('quizes/new', {quiz: quiz});
+}
+
+// GET /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+
+	// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	})	// Redirección HTTP (URL relativo) lista de preguntas
+};
+
 // GET /author
 exports.author = function(req, res) {
 	res.render('author', {});
